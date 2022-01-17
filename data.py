d3ffa0de5661446ca78ffae89c6e758396cbc359
mrcuongtroll <38219186+mrcuongtroll@users.noreply.github.com>
2022-01-17 20:39:15 +0700

    Added login and logout features

---
+++ data.py
@@ -0,0 +1,43 @@
+# Classes
+class Account:
+
+    def __init__(self, password, role='user', first_name=None, last_name=None,
+                 birth_year=None, birth_month=None, birth_day=None, address=None):
+        self.password = password
+        self.role = role
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+        self.birth_month = birth_month
+        self.birth_day = birth_day
+        self.address = address
+
+    def to_dict(self):
+        return self.__dict__
+
+
+class Task:
+
+    def __init__(self, task_name, username, date, start=None, end=None, location=None, note=None,
+                 status=None, finish_date=None, finished=False, time_overlap_tags=()):
+        self.task_name = task_name
+        self.username = username
+        self.date = date
+        self.start = start
+        self.end = end
+        self.location = location
+        self.note = note
+        self.status = status
+        self.finish_date = finish_date
+        self.finished = finished
+        self.time_overlap_tags = time_overlap_tags
+
+    def to_dict(self):
+        return self.__dict__
+
+
+# Constants:
+INIT_ACCOUNTS = {'stay_signed_in': None,
+                 'accounts': {'admin': Account(password='admin', role='admin').to_dict()}}
+INIT_TASKS = {'time_overlap_tags': [],
+              'tasks_list': []}
--- working_window.py
+++ working_window.py
@@ -1,25 +1,38 @@
 from tkinter import *
 from tkinter import ttk
 import os
+import json
 import utils
+import data
 import profile
 import accounts_manager
 import todo_list
+import login_window
 
 
 # Classes
 class _WorkingWindow(Tk):
 
-    def __init__(self, mode='user'):
+    def __init__(self, username, mode='user'):
         super().__init__()
+        self.username = username
         self.mode = mode
         self.data_path = os.path.join(os.getcwd(), 'data/')
+        self.task_path = os.path.join(self.data_path, 'tasks/')
+        with open(os.path.join(self.data_path, 'accounts.bin'), 'rb') as f:
+            self.accounts = json.loads(f.read().decode('utf-8'))
+        try:
+            with open(os.path.join(self.task_path, f'{self.username}.bin'), 'rb') as f:
+                self.tasks = json.loads(f.read().decode('utf-8'))
+        except FileNotFoundError:
+            self.tasks = data.INIT_TASKS
 
         # root window.
         self.title('ToDo List')
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
         self.geometry(f'640x480+{int(screen_width / 2 - 320)}+{int(screen_height / 2 - 240)}')
+        self.state('zoomed')
         # We need a ttk.Style object to manage the style of widgets
         self.style = ttk.Style(self)
 
@@ -30,7 +43,7 @@ class _WorkingWindow(Tk):
         self.file_menu = Menu(self.menu, tearoff=False)
         self.menu.add_cascade(label='File', menu=self.file_menu)
         self.file_menu.add_command(label='Refresh', command=None)   # TODO: this
-        self.file_menu.add_command(label='Log out', command=None)   # TODO: this
+        self.file_menu.add_command(label='Log out', command=self.logout)
         self.file_menu.add_command(label='Exit', command=None)  # TODO: this
         # TODO: add more stuff here
         # Edit menu:
@@ -43,11 +56,13 @@ class _WorkingWindow(Tk):
         self.notebook.pack(expand=True, side=LEFT, fill=BOTH, anchor=N+W)
         self.notebook.enable_traversal()    # (Shift) + Ctrl + Tab
         # To do list (The "main" working frame)
-        self.todo_frame = todo_list.create_todo_frame(self.notebook)
+        self.todo_frame = todo_list.create_todo_frame(self.notebook, self.username)
         self.notebook.add(self.todo_frame, text='To do list')
         # Profile: The user can view their profile info here
-        self.profile_frame = profile.create_profile_frame(self.notebook)
+        self.profile_frame = profile.create_profile_frame(self.notebook, self.username)
         self.notebook.add(self.profile_frame, text='Your profile')
+        self.logout_button = ttk.Button(self.profile_frame, text='Log out', command=lambda: self.logout())
+        self.logout_button.grid(row=2, column=0, sticky=E)
         # Accounts manager: For the admin to manage user accounts
         if self.mode == 'admin':
             self.accounts_manager_frame = accounts_manager.create_accounts_manager_frame(self.notebook)
@@ -56,7 +71,19 @@ class _WorkingWindow(Tk):
         # main loop
         self.mainloop()
 
+    def destroy(self):
+        with open(os.path.join(self.data_path, 'accounts.bin'), 'wb') as f:
+            f.write(json.dumps(self.accounts).encode('utf-8'))
+        with open(os.path.join(self.task_path, f'{self.username}.bin'), 'wb') as f:
+            f.write(json.dumps(self.tasks).encode('utf-8'))
+        super().destroy()
+
+    def logout(self, event=None):
+        self.accounts['stay_signed_in'] = None
+        self.destroy()
+        login_window.create_login_window()
+
 
 # Functions
-def create_working_window(mode='user'):
-    _WorkingWindow(mode=mode)
+def create_working_window(username, mode='user'):
+    _WorkingWindow(username=username, mode=mode)
